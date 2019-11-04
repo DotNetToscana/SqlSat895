afd4b9167a6589267628cb0146b1f923d1e6e835
Marco Minerva <marco.minerva@gmail.com>
2019-11-04 18:41:11 +0100

    Initial commit

---
+++ Src/DataSample.Client/Services/RemoteProductsService.cs
@@ -0,0 +1,38 @@
+﻿using DataSample.BusinessLayer.Services;
+using DataSample.Common.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSample.Client.Services
+{
+    public class RemoteProductsService : IProductsService
+    {
+        private const string BASE_URL = "https://localhost:44330/api/";
+        private readonly HttpClient client;
+
+        public RemoteProductsService()
+        {
+            client = new HttpClient { BaseAddress = new Uri(BASE_URL) };
+        }
+
+        public async Task<IEnumerable<Product>> GetAsync(string searchTerm, int pageIndex, int itemsPerPage)
+        {
+            var resource = $"Products?searchTerm={searchTerm}&pageIndex={pageIndex}&itemsPerPage={itemsPerPage}";
+            var response = await client.GetStringAsync(resource);
+            return JsonConvert.DeserializeObject<IEnumerable<Product>>(response);
+        }
+
+        public async Task<Product> SaveAsync(Product product)
+        {
+            var json = JsonConvert.SerializeObject(product);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            await client.PostAsync("Products", content);
+
+            return product;
+        }
+    }
+}
---
+++ Src/DataSample.DataAccessLayer/EntityFramework/IEntityFrameworkContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSample.DataAccessLayer.EntityFramework
+{
+    public interface IEntityFrameworkContext
+    {
+        IQueryable<T> GetData<T>(bool trackingChanges = false) where T : class;
+
+        void Insert<T>(T entity) where T : class;
+
+        void Update<T>(T entity) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        Task SaveAsync();
+    }
+}
